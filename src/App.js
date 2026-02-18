3f3b12cfbe7f145789fb68b307025454bda0360e
Zakariyae Mahou <zakariyaemahou@gmail.com>
2026-02-18 09:43:56 +0100

    small changes to push project in dd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ConsumerRoutes from "./routes/ConsumerRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
